265ca653057fc5c8e6d70ec8df7466df742286bf
Cole Bemis <colebemis@gmail.com>
2018-04-14 22:10:07 -0700

    Add me resolver function

--- server/src/resolvers/Query/me.ts
+++ server/src/resolvers/Query/me.ts
@@ -1,6 +1,8 @@
 import { Context, getUserId } from '../../utils'
 
-export const me = async (root, args, context: Context, info) => {
+async function me(root, args, context: Context, info) {
   const id = getUserId(context)
   return await context.db.query.user({ where: { id } }, info)
 }
+
+export default me
--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,9 +1,11 @@
+import me from './Query/me'
 import boards from './Query/boards'
 import createBoard from './Mutation/createBoard'
 import authenticate from './Mutation/authenticate'
 
 const resolvers = {
   Query: {
+    me,
     boards,
   },
   Mutation: {

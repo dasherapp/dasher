990ee47072349b2689be5a317aace39846954c62
Cole Bemis <colebemis@gmail.com>
2018-04-16 23:19:25 -0700

    Reset body margin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { css } from 'glamor'
 
 import App from './components/App'
+
+css.global('body', { margin: 0 })
 
 ReactDOM.render(<App />, document.getElementById('root'))

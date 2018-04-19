451e09c60cfc17908e103e391f954223c3e4f602
Lexa Hall <lexahall1@gmail.com>
2018-04-19 00:13:03 -0700

    Finish set up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { css } from 'glamor'
+import Modal from 'react-modal'
 
 import App from './components/App'
 
 css.global('body', { margin: 0 })
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootSelector = '#root'
+Modal.setAppElement(rootSelector)
+ReactDOM.render(<App />, document.querySelector(rootSelector))

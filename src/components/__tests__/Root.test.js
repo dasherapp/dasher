3b42bc5ebaea452bb0c1c8de6d592a6bb745f027
Cole Bemis <colebemis@gmail.com>
2018-04-19 20:02:07 -0700

    Remove Root test

--- src/components/__tests__/Root.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-
-import Root from '../Root'
-
-it('renders without crashing', () => {
-  const tree = renderer.create(<Root />).toJSON()
-  expect(tree).toMatchSnapshot()
-})

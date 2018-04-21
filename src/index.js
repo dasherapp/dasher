f07483b0081e28d5c2f70b47d22440e13ee08106
Lexa Hall <lexahall1@gmail.com>
2018-04-21 01:02:41 -0700

    Set body background color

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom'
 import { css } from 'glamor'
 import Modal from 'react-modal'
 
-import { fonts } from './theme'
+import { fonts, colors } from './theme'
 import App from './components/App'
 
 css.global('body', {
+  boxSizing: 'border-box',
   margin: 0,
   fontFamily: fonts.sans,
-  boxSizing: 'border-box',
+  backgroundColor: colors.gray[1],
 })
 
 css.global('*, *:before, *:after', {

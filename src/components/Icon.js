b937bc5226623e4f129fd00fb2e76fd099ddf19f
Lexa Hall <lexahall1@gmail.com>
2018-04-20 21:59:17 -0700

    Add icons

---
+++ src/components/Icon.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import { oneOfType, number, string } from 'prop-types'
+
+function Icon({ size, ...props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    />
+  )
+}
+
+Icon.propTypes = {
+  size: oneOfType([number, string]),
+}
+
+Icon.defaultProps = {
+  size: 24,
+}
+
+export function IssueIcon(props) {
+  return (
+    <Icon {...props}>
+      <circle cx="12" cy="12" r="10" />
+      <line x1="12" y1="8" x2="12" y2="12" />
+      <line x1="12" y1="16" x2="12" y2="16" />
+    </Icon>
+  )
+}
+
+export function PullRequestIcon(props) {
+  return (
+    <Icon {...props}>
+      <circle cx="18" cy="18" r="3" />
+      <circle cx="6" cy="6" r="3" />
+      <path d="M13 6h3a2 2 0 0 1 2 2v7" />
+      <line x1="6" y1="9" x2="6" y2="21" />
+    </Icon>
+  )
+}
+
+export function MergeIcon(props) {
+  return (
+    <Icon {...props}>
+      <circle cx="18" cy="18" r="3" />
+      <circle cx="6" cy="6" r="3" />
+      <path d="M6 21V9a9 9 0 0 0 9 9" />
+    </Icon>
+  )
+}
+
+export function CheckIcon(props) {
+  return (
+    <Icon {...props}>
+      <polyline points="20 6 9 17 4 12" />
+    </Icon>
+  )
+}
+
+export function XIcon(props) {
+  return (
+    <Icon {...props}>
+      <line x1="18" y1="6" x2="6" y2="18" />{' '}
+      <line x1="6" y1="6" x2="18" y2="18" />
+    </Icon>
+  )
+}
+
+export function CircleIcon(props) {
+  return (
+    <Icon {...props}>
+      <circle cx="12" cy="12" r="10" />
+    </Icon>
+  )
+}
+
+export function Ellipses(props) {
+  return (
+    <Icon>
+      <circle cx="12" cy="12" r="1" />
+      <circle cx="19" cy="12" r="1" />
+      <circle cx="5" cy="12" r="1" />
+    </Icon>
+  )
+}
+
+export default Icon

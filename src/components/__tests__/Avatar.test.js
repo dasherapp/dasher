3d613101355c0503cb63fba357c28d4bd8b91f7a
Lexa Hall <lexahall1@gmail.com>
2018-04-21 22:43:39 -0700

    Update placeholders

--- src/components/__tests__/Avatar.test.js
+++ src/components/__tests__/Avatar.test.js
@@ -1,38 +1,44 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { render, container } from 'react-testing-library'
 import Avatar from '../Avatar'
 
 it('renders without crashing', () => {
-  const tree = renderer
-    .create(
-      <Avatar src="http://via.placeholder.com/100x100" alt="placeholder" />,
-    )
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+  const { container } = render(
+    <Avatar src="http://via.placeholder.com/100x100" alt="placeholder" />,
+  )
+  expect(container.firstChild).toMatchSnapshot()
 })
 
 it('renders with custom size (number)', () => {
-  const tree = renderer
-    .create(
-      <Avatar
-        src="http://via.placeholder.com/100x100"
-        alt="placeholder"
-        size={32}
-      />,
-    )
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+  const { container } = render(
+    <Avatar
+      src="http://via.placeholder.com/100x100"
+      alt="placeholder"
+      size={32}
+    />,
+  )
+  expect(container.firstChild).toMatchSnapshot()
 })
 
 it('renders with custom size (string)', () => {
-  const tree = renderer
-    .create(
-      <Avatar
-        src="http://via.placeholder.com/100x100"
-        alt="placeholder"
-        size="4rem"
-      />,
-    )
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+  const { container } = render(
+    <Avatar
+      src="http://via.placeholder.com/100x100"
+      alt="placeholder"
+      size="4rem"
+    />,
+  )
+  expect(container.firstChild).toMatchSnapshot()
+})
+
+it('renders as circle', () => {
+  const { container } = render(
+    <Avatar
+      src="http://via.placeholder.com/100x100"
+      alt="placeholder"
+      size="4rem"
+      shape="circle"
+    />,
+  )
+  expect(container.firstChild).toMatchSnapshot()
 })

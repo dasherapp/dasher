b5cb353fd5a09c96681bd8e4c9f9ea72122f4ea8
Cole Bemis <colebemis@gmail.com>
2018-04-21 17:55:15 -0700

    Update Icon tests

--- src/components/__tests__/Icon.test.js
+++ src/components/__tests__/Icon.test.js
@@ -11,30 +11,40 @@ import Icon, {
 } from '../Icon'
 
 it('renders without crashing', () => {
-  const tree = renderer.create(<Icon />).toJSON()
+  const tree = renderer
+    .create(
+      <Icon>
+        <circle cx="12" cy="12" r="10" />
+      </Icon>,
+    )
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 it('renders with custom color and size', () => {
-  const tree = renderer.create(<Icon color="#C27E00" size={16} />).toJSON()
+  const tree = renderer
+    .create(
+      <Icon color="#C27E00" size={16}>
+        <circle cx="12" cy="12" r="10" />
+      </Icon>,
+    )
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
 
-describe('Custom icons', () => {
-  it('renders without crashing', () => {
-    const tree = renderer
-      .create(
-        <div>
-          <IssueIcon />
-          <PullRequestIcon />
-          <MergeIcon />
-          <CheckIcon />
-          <XIcon />
-          <CircleIcon />
-          <EllipsesIcon />
-        </div>,
-      )
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+it('renders custom icons', () => {
+  const tree = renderer
+    .create(
+      <div>
+        <IssueIcon />
+        <PullRequestIcon />
+        <MergeIcon />
+        <CheckIcon />
+        <XIcon />
+        <CircleIcon />
+        <EllipsesIcon />
+      </div>,
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
 })

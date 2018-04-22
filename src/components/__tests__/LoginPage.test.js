2577246efd9f068ba90b7da1c4de8822b25a227a
Cole Bemis <colebemis@gmail.com>
2018-04-22 10:41:00 -0700

    Write LoginPage tests

--- src/components/__tests__/LoginPage.test.js
+++ src/components/__tests__/LoginPage.test.js
@@ -1,7 +1,36 @@
+import React, { Fragment } from 'react'
+import { render } from 'react-testing-library'
+import { MemoryRouter, Route } from 'react-router-dom'
+import 'dom-testing-library/extend-expect'
+
+import { AUTH_TOKEN_KEY } from '../../utils/auth'
 import LoginPage from '../LoginPage'
 
-it('redirects when logged in')
+function renderRoutes() {
+  return render(
+    <MemoryRouter initialEntries={['/login']}>
+      <Fragment>
+        <Route exact path="/" component={() => <div>Example Page</div>} />
+        <Route exact path="/login" component={LoginPage} />
+      </Fragment>
+    </MemoryRouter>,
+  )
+}
 
-it('renders correctly when logged out')
+it('redirects to / when logged in', () => {
+  localStorage.setItem(AUTH_TOKEN_KEY, 'fake_auth_token')
+
+  const { queryByText } = renderRoutes()
+
+  expect(queryByText('Example Page')).toBeInTheDOM()
+
+  localStorage.clear()
+})
+
+it("doesn't redirect when logged out", () => {
+  const { queryByText } = renderRoutes()
+
+  expect(queryByText('Example Page')).not.toBeInTheDOM()
+})
 
 it('sets window.location correctly after login button is clicked')

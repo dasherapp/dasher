7e05a9a417120e3589630289dfce0591a8fae7f7
Cole Bemis <colebemis@gmail.com>
2018-04-21 17:19:51 -0700

    Convert utils to arrow functions

--- src/utils/style.js
+++ src/utils/style.js
@@ -1,30 +1,24 @@
 import color from 'color-string'
 import memoize from 'fast-memoize'
 
-function isNumber(value) {
-  return typeof value === 'number' && !isNaN(value)
-}
+const isNumber = value => typeof value === 'number' && !isNaN(value)
 
-function toUnit(unit) {
-  return value => (isNumber(value) && value !== 0 ? value + unit : value)
-}
+const toUnit = unit => value =>
+  isNumber(value) && value !== 0 ? value + unit : value
 
 export const toPx = toUnit('px')
 
 export const toEm = toUnit('em')
 
-export function toMediaQuery(breakpoint) {
-  return `@media screen and (min-width: ${toEm(breakpoint)})`
-}
+export const toMediaQuery = breakpoint =>
+  `@media screen and (min-width: ${toEm(breakpoint)})`
 
-export function joinSpacing() {
-  if (arguments.length > 4) {
+export const joinSpacing = (...args) => {
+  if (args.length > 4) {
     throw new Error('Too many arguments')
   }
 
-  return Array.from(arguments)
-    .map(toPx)
-    .join(' ')
+  return args.map(toPx).join(' ')
 }
 
 /**
@@ -39,43 +33,43 @@ export function joinSpacing() {
  */
 export const toAlpha = memoize(
   (foreground, background = '#fff') => {
-  const r = 0
-  const g = 1
-  const b = 2
+    const r = 0
+    const g = 1
+    const b = 2
 
-  const fgColor = color.get.rgb(foreground)
-  const bgColor = color.get.rgb(background)
+    const fgColor = color.get.rgb(foreground)
+    const bgColor = color.get.rgb(background)
 
-  let bestAlpha = [r, g, b]
-    .map(
-      channel =>
-        (fgColor[channel] - bgColor[channel]) /
-        ((0 < fgColor[channel] - bgColor[channel] ? 255 : 0) -
-          bgColor[channel]),
-    )
-    .sort(function(a, b) {
-      return b - a
-    })[0]
-
-  bestAlpha = Math.max(Math.min(bestAlpha, 1), 0)
-
-  // Calculate the resulting color
-  function processChannel(channel) {
-    if (0 === bestAlpha) {
-      return bgColor[channel]
-    } else {
-      return (
-        bgColor[channel] + (fgColor[channel] - bgColor[channel]) / bestAlpha
+    let bestAlpha = [r, g, b]
+      .map(
+        channel =>
+          (fgColor[channel] - bgColor[channel]) /
+          ((0 < fgColor[channel] - bgColor[channel] ? 255 : 0) -
+            bgColor[channel]),
       )
-    }
-  }
+      .sort(function(a, b) {
+        return b - a
+      })[0]
 
-  return color.to.rgb(
-    processChannel(r),
-    processChannel(g),
-    processChannel(b),
-    Math.round(bestAlpha * 100) / 100,
-  )
+    bestAlpha = Math.max(Math.min(bestAlpha, 1), 0)
+
+    // Calculate the resulting color
+    function processChannel(channel) {
+      if (0 === bestAlpha) {
+        return bgColor[channel]
+      } else {
+        return (
+          bgColor[channel] + (fgColor[channel] - bgColor[channel]) / bestAlpha
+        )
+      }
+    }
+
+    return color.to.rgb(
+      processChannel(r),
+      processChannel(g),
+      processChannel(b),
+      Math.round(bestAlpha * 100) / 100,
+    )
   },
   {
     strategy: memoize.strategies.variadic,

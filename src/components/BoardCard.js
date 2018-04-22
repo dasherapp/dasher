72f015e8aac25982a0ff77803123a067816b9f41
Lexa Hall <lexahall1@gmail.com>
2018-04-21 23:07:07 -0700

    Make BoardCardContainer CardLink

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -8,7 +8,7 @@ import ModalContainer from '../containers/ModalContainer'
 import UpdateBoardModal from './UpdateBoardModal'
 import DeleteBoardModal from './DeleteBoardModal'
 
-const BoardCardContainer = glamorous.div({
+const CardLink = glamorous(Link)({
   height: 160,
   padding: spacing[2],
   fontSize: fontSizes[3],
@@ -21,31 +21,29 @@ function BoardCard({ board }) {
   return (
     <Subscribe to={[ModalContainer]}>
       {modal => (
-        <Link to={`/board/${board.id}`}>
-          <BoardCardContainer>
-            {board.name}
-            <button
-              onClick={event => {
-                event.preventDefault()
-                modal.openModal(UpdateBoardModal, {
-                  board,
-                })
-              }}
-            >
-              Edit board
-            </button>
-            <button
-              onClick={event => {
-                event.preventDefault()
-                modal.openModal(DeleteBoardModal, {
-                  board: board,
-                })
-              }}
-            >
-              Delete board
-            </button>
-          </BoardCardContainer>
-        </Link>
+        <CardLink to={`/board/${board.id}`}>
+          {board.name}
+          <button
+            onClick={event => {
+              event.preventDefault()
+              modal.openModal(UpdateBoardModal, {
+                board,
+              })
+            }}
+          >
+            Edit board
+          </button>
+          <button
+            onClick={event => {
+              event.preventDefault()
+              modal.openModal(DeleteBoardModal, {
+                board: board,
+              })
+            }}
+          >
+            Delete board
+          </button>
+        </CardLink>
       )}
     </Subscribe>
   )

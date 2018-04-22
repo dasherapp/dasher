169e3a9d26dbb8050f5ef6016ee2b1c9b5c53737
Cole Bemis <colebemis@gmail.com>
2018-04-21 22:21:17 -0700

    Add PrivateRoute tests

---
+++ src/components/__tests__/PrivateRoute.test.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from 'react'
+import { render } from 'react-testing-library'
+import { MemoryRouter, Route } from 'react-router-dom'
+import 'dom-testing-library/extend-expect'
+
+import { AUTH_TOKEN_KEY } from '../../utils/auth'
+import PrivateRoute from '../PrivateRoute'
+
+function renderRoutes() {
+  return render(
+    <MemoryRouter initialEntries={['/']}>
+      <Fragment>
+        <PrivateRoute
+          exact
+          path="/"
+          component={() => <div>Example Page</div>}
+        />
+        <Route exact path="/login" component={() => <div>Login Page</div>} />
+      </Fragment>
+    </MemoryRouter>,
+  )
+}
+
+it('renders correctly when logged in', () => {
+  localStorage.setItem(AUTH_TOKEN_KEY, 'fake_auth_token')
+
+  const { queryByText } = renderRoutes()
+
+  expect(queryByText('Example Page')).toBeInTheDOM()
+  expect(queryByText('Login Page')).not.toBeInTheDOM()
+
+  localStorage.clear()
+})
+
+it('redirects to /login when logged out', () => {
+  const { queryByText } = renderRoutes()
+
+  expect(queryByText('Example Page')).not.toBeInTheDOM()
+  expect(queryByText('Login Page')).toBeInTheDOM()
+})

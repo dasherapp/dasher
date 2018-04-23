1f0557aac845fa5f64970c0711f99fd9be7b83fd
Lexa Hall <lexahall1@gmail.com>
2018-04-23 10:46:26 -0700

    Add tests for SkeletonButton

---
+++ src/components/__tests__/SkeletonButton.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { renderIntoDocument, render, fireEvent } from 'react-testing-library'
+import SkeletonButton from '../SkeletonButton'
+
+it('renders without crashing', () => {
+  const { container } = render(
+    <SkeletonButton>Test SkeletonButton</SkeletonButton>,
+  )
+  expect(container.firstChild).toMatchSnapshot()
+})
+
+it('renders with custom width', () => {
+  const { container } = render(
+    <SkeletonButton width={360}>Custom Width SkeletonButton</SkeletonButton>,
+  )
+  expect(container.firstChild).toMatchSnapshot()
+})
+
+it('calls onClick when button is clicked', () => {
+  const handleClick = jest.fn()
+  const { getByText } = renderIntoDocument(
+    <SkeletonButton onClick={handleClick}>
+      Clickable SkeletonButton
+    </SkeletonButton>,
+  )
+  fireEvent(
+    getByText('Clickable SkeletonButton'),
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+    }),
+  )
+
+  expect(handleClick).toHaveBeenCalledTimes(1)
+})

b99016031c40d9ab32b505c80cf7f9d6a2a5e8e6
Cole Bemis <colebemis@gmail.com>
2018-04-22 23:05:16 -0700

    Create Dropdown component

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -21,6 +21,7 @@ import Button from './Button'
 import { EllipsesIcon } from './Icon'
 import Flex from './Flex'
 import Spacer from './Spacer'
+import Dropdown, { MenuItem } from './Dropdown'
 
 const CardLink = glamorous(Link)({
   padding: spacing[3],
@@ -54,26 +55,31 @@ function BoardCard({ board }) {
           <Flex alignItems="center" paddingLeft={spacing[1]}>
             <span>{board.name}</span>
             <Spacer />
-            <Button kind="icon" onClick={event => event.preventDefault()}>
-              <EllipsesIcon />
-            </Button>
+            <Dropdown
+              toggleComponent={({ onClick }) => (
+                <Button kind="icon" onClick={onClick}>
+                  <EllipsesIcon />
+                </Button>
+              )}
+            >
+              <MenuItem
+                onClick={event => {
+                  event.preventDefault()
+                  modal.openModal(UpdateBoardModal, { board })
+                }}
+              >
+                Edit board
+              </MenuItem>
+              <MenuItem
+                onClick={event => {
+                  event.preventDefault()
+                  modal.openModal(DeleteBoardModal, { board })
+                }}
+              >
+                Delete board
+              </MenuItem>
+            </Dropdown>
           </Flex>
-          {/* <button
-            onClick={event => {
-              event.preventDefault()
-              modal.openModal(UpdateBoardModal, { board })
-            }}
-          >
-            Edit board
-          </button>
-          <button
-            onClick={event => {
-              event.preventDefault()
-              modal.openModal(DeleteBoardModal, { board })
-            }}
-          >
-            Delete board
-          </button> */}
         </CardLink>
       )}
     </Subscribe>
---
+++ src/components/Dropdown.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import glamorous, { Div } from 'glamorous'
+
+import {
+  spacing,
+  colors,
+  radii,
+  fontSizes,
+  lineHeights,
+  shadows,
+} from '../theme'
+import { joinSpacing, toAlpha } from '../utils/style'
+
+const Relative = glamorous.div({
+  position: 'relative',
+})
+
+const Menu = glamorous.div({
+  position: 'absolute',
+  right: 0,
+  display: 'flex',
+  flexDirection: 'column',
+  padding: joinSpacing(spacing[0], 0),
+  backgroundColor: colors.gray[8],
+  borderRadius: radii[1],
+  boxShadow: shadows[2],
+})
+
+export const MenuItem = glamorous.button({
+  padding: joinSpacing(spacing[1], spacing[3]),
+  fontFamily: 'inherit',
+  fontSize: fontSizes[1],
+  lineHeight: lineHeights.tight,
+  textAlign: 'left',
+  color: colors.white,
+  backgroundColor: 'transparent',
+  border: 'none',
+  cursor: 'pointer',
+  outline: 0,
+  whiteSpace: 'nowrap',
+
+  ':hover,:focus': {
+    backgroundColor: toAlpha(colors.gray[7], colors.gray[8]),
+  },
+})
+
+class Dropdown extends Component {
+  state = {
+    isOpen: false,
+  }
+
+  toggleOpen = () => this.setState({ isOpen: !this.state.isOpen })
+
+  render() {
+    const { toggleComponent: Toggle, children } = this.props
+    const { isOpen } = this.state
+    return (
+      <Relative>
+        <Toggle
+          onClick={event => {
+            event.preventDefault()
+            this.toggleOpen()
+          }}
+        />
+        {isOpen && <Menu>{children}</Menu>}
+      </Relative>
+    )
+  }
+}
+
+export default Dropdown

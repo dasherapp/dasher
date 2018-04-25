5979302e21219cafe9d3227536457ca626fae731
Lexa Hall <lexahall1@gmail.com>
2018-04-24 18:50:00 -0700

    Move DragDropContext out of App

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'unstated'
 
 import Apollo from './Apollo'
-import DragDrop from './DragDrop'
 import PrivateRoute from './PrivateRoute'
 import ModalRoot from './ModalRoot'
 import HomePage from './HomePage'
@@ -16,20 +15,18 @@ function App() {
   return (
     <Apollo>
       <Provider>
-        <DragDrop>
-          <Router>
-            <Fragment>
-              <ModalRoot />
-              <Switch>
-                <PrivateRoute exact path="/" component={HomePage} />
-                <PrivateRoute path="/board/:id" component={BoardPage} />
-                <Route exact path="/login" component={LoginPage} />
-                <Route exact path="/callback" component={CallbackPage} />
-                <Route component={NotFoundPage} />
-              </Switch>
-            </Fragment>
-          </Router>
-        </DragDrop>
+        <Router>
+          <Fragment>
+            <ModalRoot />
+            <Switch>
+              <PrivateRoute exact path="/" component={HomePage} />
+              <PrivateRoute path="/board/:id" component={BoardPage} />
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/callback" component={CallbackPage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </Fragment>
+        </Router>
       </Provider>
     </Apollo>
   )

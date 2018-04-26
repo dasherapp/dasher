aba22f389a5184963953e0057e230cc24e6625b5
Lexa Hall <lexahall1@gmail.com>
2018-04-26 07:27:16 -0700

    Add GITHUB_TOKEN_KEY to local storage when user signs in

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,11 +3,13 @@ import { gql } from 'apollo-boost'
 import { client } from '../components/Apollo'
 
 export const AUTH_TOKEN_KEY = 'auth_token'
+export const GITHUB_TOKEN_KEY = 'github_token'
 
 const AUTHENTICATE_MUTATION = gql`
   mutation AuthenticateMutation($githubCode: String!) {
     authenticate(githubCode: $githubCode) {
       token
+      githubToken
     }
   }
 `
@@ -19,10 +21,12 @@ export async function logIn(githubCode) {
   })
 
   setAuthToken(data.authenticate.token)
+  setGithubToken(data.authenticate.githubToken)
 }
 
 export function logOut() {
   removeAuthToken()
+  removeGithubToken()
 }
 
 export function isLoggedIn() {
@@ -39,4 +43,16 @@ export function getAuthToken() {
 
 export function removeAuthToken() {
   localStorage.removeItem(AUTH_TOKEN_KEY)
+}
+
+export function setGithubToken(githubToken) {
+  localStorage.setItem(GITHUB_TOKEN_KEY, githubToken)
+}
+
+export function getGithubToken() {
+  return localStorage.getItem(GITHUB_TOKEN_KEY)
+}
+
+export function removeGithubToken() {
+  localStorage.removeItem(GITHUB_TOKEN_KEY)
 }

1b69fbdd989071d728cdcb0ea8ad8d02c153ff9d
Cole Bemis <colebemis@gmail.com>
2018-04-27 12:46:50 -0700

    Update avatar button

--- src/components/AvatarButton.js
+++ src/components/AvatarButton.js
@@ -2,21 +2,32 @@ import React from 'react'
 import { oneOfType, oneOf, string, number } from 'prop-types'
 import glamorous from 'glamorous'
 
+import { colors, transition, radii } from '../theme'
+import { toAlpha } from '../utils/style'
 import Avatar from './Avatar'
 
-const UnstyledButton = glamorous.button({
+const UnstyledButton = glamorous.button(props => ({
   display: 'inline-block',
   margin: 0,
   padding: 0,
   lineHeight: 0,
   background: 'transparent',
   border: 0,
+  outline: 0,
   cursor: 'pointer',
-})
+  borderRadius: props.shape === 'square' ? radii[0] : '50%',
+  transitionProperty: 'box-shadow',
+  transitionDuration: transition.duration,
+  transitionTimingFunction: transition.easing,
+
+  ':focus': {
+    boxShadow: `0 0 0 3px ${toAlpha(colors.gray[4])}`,
+  },
+}))
 
 const AvatarButton = React.forwardRef(({ src, size, shape, ...props }, ref) => {
   return (
-    <UnstyledButton innerRef={ref} {...props}>
+    <UnstyledButton innerRef={ref} shape={shape} {...props}>
       <Avatar src={src} size={size} shape={shape} />
     </UnstyledButton>
   )

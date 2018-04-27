98d702b7cbb353fc1347a7376d1c69230c793a53
Cole Bemis <colebemis@gmail.com>
2018-04-27 12:54:12 -0700

    Update import order

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
+import { spacing } from '../theme'
 import PageContainer from './PageContainer'
 import AccountMenu from './AccountMenu'
 import Boards from './Boards'
 import Flex from './Flex'
 import Spacer from './Spacer'
-import { spacing } from '../theme'
 
 function HomePage() {
   return (

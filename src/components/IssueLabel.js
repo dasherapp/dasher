02ef2259214866ed44251060f85b0c8da635c3b9
Lexa Hall <lexahall1@gmail.com>
2018-05-14 08:29:21 -0700

    Calculate readable text color

--- src/components/IssueLabel.js
+++ src/components/IssueLabel.js
@@ -11,6 +11,21 @@ import {
 } from '../theme'
 import { cleanElement } from '../utils/utils'
 
+function getReadableColor(hex) {
+  // Color brightness is determined by the following formula:
+  // ((Red value X 299) + (Green value X 587) + (Blue value X 114)) / 1000
+  // Source: https://www.w3.org/TR/AERT/#color
+
+  // Lower threshold means more dark text on dark background
+  const threshold = 136 // slightly above half of 256, to favor white text
+
+  const rgb = color.get.rgb(hex)
+
+  const brightness = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000
+
+  return brightness > threshold ? '#000000' : '#ffffff'
+}
+
 const Label = styled(cleanElement({ type: 'span', excludeProps: ['color'] }))(
   props => ({
     display: 'inline-block',
@@ -20,7 +35,7 @@ const Label = styled(cleanElement({ type: 'span', excludeProps: ['color'] }))(
     fontSize: fontSizes[0],
     fontWeight: fontWeights.bold,
     lineHeight: lineHeights.none,
-    color: colors.black,
+    color: getReadableColor(props.color),
     backgroundColor: props.color,
     borderRadius: radii[0],
     boxShadow: 'inset 0 -1px 0 rgba(0, 0, 0, 0.10)',
